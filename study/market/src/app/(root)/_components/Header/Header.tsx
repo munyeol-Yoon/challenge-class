5d61caa8a672cebe49221c17e0cadd91130d3f41
muyeon <gmakin36@gmail.com>
2024-06-26 17:19:49 +0900

    [study] market update link

--- study/market/src/app/(root)/_components/Header/Header.tsx
+++ study/market/src/app/(root)/_components/Header/Header.tsx
@@ -16,7 +16,7 @@ function Header() {
               <Link href="/deals/create">판매하기</Link>
             </li>
             <li>
-              <Link href="/my-deals">내 판매글</Link>
+              <Link href="/my/deals">내 판매글</Link>
             </li>
           </ul>
         </nav>
